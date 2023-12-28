1e38bb8cd1c59941d174d964e9b80cb6296f3ff6
Sahara <sahara.tm65@gmail.com>
2023-12-28 12:53:20 -0500

    Added code to delete method and added white spacing for readability

--- EasyShopAPI/EasyShopAPI/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ EasyShopAPI/EasyShopAPI/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -26,11 +26,14 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
         List<Category> categories = new ArrayList<>();
 
         try (Connection connection = getConnection()) {
+
             PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM categories");
             ResultSet resultSet = preparedStatement.executeQuery();
+
             while (resultSet.next()) {
                 categories.add(mapRow(resultSet));
             }
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -41,6 +44,7 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
     public Category getById(int categoryId) {
 
         try (Connection connection = getConnection()) {
+
             PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM categories WHERE category_id = ?");
             preparedStatement.setInt(1, categoryId);
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -48,6 +52,7 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
             if (resultSet.next()) {
                 return mapRow(resultSet);
             }
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -58,10 +63,12 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
     public Category create(Category category) {
 
         try (Connection connection = getConnection()) {
+
             PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO categories (name, description) VALUES (?, ?)");
             preparedStatement.setString(1, category.getName());
             preparedStatement.setString(2, category.getDescription());
             preparedStatement.executeUpdate();
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -70,13 +77,15 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
 
     @Override
     public void update(int categoryId, Category category) {
-        // update category
+
         try (Connection connection = getConnection()) {
+
             PreparedStatement preparedStatement = connection.prepareStatement("UPDATE categories SET name = ?, description = ? WHERE category_id = ?");
             preparedStatement.setString(1, category.getName());
             preparedStatement.setString(2, category.getDescription());
             preparedStatement.setInt(3, categoryId);
             preparedStatement.executeUpdate();
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -84,24 +93,29 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
 
     @Override
     public void delete(int categoryId) {
-        // delete category
+
         try (Connection connection = getConnection()) {
 
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM categories WHERE category_id = ?");
+            preparedStatement.setInt(1, categoryId);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
     private Category mapRow(ResultSet row) throws SQLException {
+
         int categoryId = row.getInt("category_id");
         String name = row.getString("name");
         String description = row.getString("description");
 
-        Category category = new Category() {{
+        return new Category() {{
             setCategoryId(categoryId);
             setName(name);
             setDescription(description);
         }};
-
-        return category;
     }
 
 }
