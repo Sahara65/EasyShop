7deb1128200d06e0c00b42be83f396f774ffe2c0
Sahara <sahara.tm65@gmail.com>
2024-01-01 01:43:50 -0500

    Added to ShoppingCartDao

--- EasyShopAPI/EasyShopAPI/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ EasyShopAPI/EasyShopAPI/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -4,5 +4,8 @@ import org.yearup.models.ShoppingCart;
 
 public interface ShoppingCartDao {
     ShoppingCart getByUserId(int userId);
-    // add additional method signatures here
+    ShoppingCart addItem(int productId, int userId);
+    ShoppingCart updateProductInCart(int productId, int userId, int quantity);
+    ShoppingCart removeItem(int productId, int userId);
+    ShoppingCart clearCart(int userId);
 }
